e8bd188ba19db74416774563144eb917a5f37119
David Cuero <W5845542@student.hccs.edu>
2021-05-20 15:14:27 -0500

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,21 @@
+package com.qa.main;
+
+public class Runner {
+
+	// attribute
+	public static String msg = "Welcome to Sarah's Garage";
+
+	// Main method - starting
+	public static void main(String[] args) {
+
+		System.out.println(msg);
+
+	}
+
+	// Methods
+	// accessor, static/non, return type, nameof, parameterlist, scope
+
+// class
+// attributes(class members)
+// methods (class members)
+}
