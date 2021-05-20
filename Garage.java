3a9f3e31d516b012394e802967890e35eb27bb09
David Cuero <W5845542@student.hccs.edu>
2021-05-20 15:18:31 -0500

    Add files via upload

---
+++ Garage.java
@@ -0,0 +1,23 @@
+package com.qa.main.garage;
+
+public class Garage {
+
+	// ***********//
+	// ATTRIBUTES //
+	// ***********//
+	private String garageName;
+	private int doorCount;
+
+	// ************//
+	// CONSTRUCTOR //
+	// ************//
+
+	// ********//
+	// METHODS //
+	// ********//
+
+	public String getGarageName() {
+		return garageName;
+	}
+
+}
